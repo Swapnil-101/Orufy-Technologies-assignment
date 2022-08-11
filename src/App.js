3d9fbb50e3c26c500a6e8291fcf658f6d514e428
Swapnil-101 <Swapniltiwari47@gmail.com>
2022-08-11 18:03:08 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import FilterCompo from "./components/FilterCompo";
+import MainProduct from "./components/MainProduct";
+import NavBar from "./components/NavBar";
 
-function App() {
+const App = () => {
+  const [search, setSearch] = React.useState("");
+  const [count, setCount] = React.useState(0);
+  const [sort, setSort] = React.useState("");
+  const [filters, setFilters] = React.useState({
+    brand: "",
+    category: "",
+    price: 0,
+  });
+
+  React.useEffect(() => {
+    if (count === 0) {
+      setSort("lh");
+    }
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <NavBar search={search} setSearch={setSearch} />
+      <div className="flex justify-center gap-[2rem]  my-[2rem]">
+        <div className="rounded-md drop-shadow-lg ">
+          <FilterCompo filters={filters} setFilters={setFilters} />
+        </div>
+        <MainProduct
+          search={search}
+          price={filters?.price ?? 0}
+          brand={filters?.brand ?? ""}
+          category={filters?.category ?? ""}
+          sort={sort}
+          setSort={setSort}
+        />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
